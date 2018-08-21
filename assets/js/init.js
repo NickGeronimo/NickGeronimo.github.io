f9e873fec6c00f8d29a7ed4f5d51e9d548ebf89b
Nick <Nicholas.Geronimo@gmail.com>
2018-08-20 23:38:07 -0400

    cleanup

--- assets/js/init.js
+++ assets/js/init.js
@@ -18,12 +18,7 @@ $(document).ready(function(){
   $('.tooltipped').tooltip();
   $('.parallax').parallax();
   $('.modal').modal();
-  $('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 240
-    edge: 'right', // Choose the horizontal origin
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  }
-);
+ 
 
 });
 
