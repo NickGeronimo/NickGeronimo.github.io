3ca8ad338852232ef189422a75ef75003860bc83
Nick <Nicholas.Geronimo@gmail.com>
2018-08-20 23:08:42 -0400

    modals

--- assets/js/init.js
+++ assets/js/init.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
   $('.collapsible').collapsible();
   $('.tooltipped').tooltip();
   $('.parallax').parallax();
+  $('.modal').modal();
   $('.button-collapse').sideNav({
     menuWidth: 300, // Default is 240
     edge: 'right', // Choose the horizontal origin
